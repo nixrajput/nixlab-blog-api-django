67c831f4a30b7e71512f210df81857c4c092f1e5
nixrajput <nkr.nikhil.nkr@gmail.com>
2020-09-06 03:03:20 +0530

    updated settings

--- blogapi/urls.py
+++ blogapi/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('', include('blog.urls', 'blog_api')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
