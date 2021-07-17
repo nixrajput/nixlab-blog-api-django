61c87450b05822718f32e85e448b9938fdbc160a
nixrajput <nkr.nikhil.nkr@gmail.com>
2021-07-17 20:53:16 +0530

    minor bug fixes

--- blogapi/settings.py
+++ blogapi/settings.py
@@ -130,7 +130,7 @@ else:
     AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
     AWS_DEFAULT_ACL = None
     AWS_S3_CUSTOM_DOMAIN = os.getenv('AWS_S3_CUSTOM_DOMAIN')
-    AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=100'}
+    AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=1000'}
 
     AWS_LOCATION = 'static'
 
