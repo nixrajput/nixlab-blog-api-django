47fe741bf29ca3e1b90373dd4674eb12c65c2451
nixrajput <nkr.nikhil.nkr@gmail.com>
2021-07-21 02:02:38 +0530

    some bug fixes

--- account/models.py
+++ account/models.py
@@ -12,7 +12,7 @@ from rest_framework.authtoken.models import Token
 
 
 class MyAccountManager(BaseUserManager):
-    def create_user(self, email, username, password=None):
+    def create_user(self, first_name, last_name, email, username, password=None):
         if not email:
             raise ValueError('Users must have an email address')
         if not username:
@@ -21,18 +21,22 @@ class MyAccountManager(BaseUserManager):
         user = self.model(
             email=self.normalize_email(email),
             username=username,
+            first_name=first_name,
+            last_name=last_name
         )
 
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, username, email, password):
+    def create_superuser(self, first_name, last_name, username, email, password):
         user = self.create_user(
             username=username,
             email=self.normalize_email(email),
-            password=password,
+            first_name=first_name,
+            last_name=last_name
         )
+        user.set_password(password)
         user.is_admin = True
         user.is_staff = True
         user.is_superuser = True
